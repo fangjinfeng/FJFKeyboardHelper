d5272d77c3f41c287e5f6f57a52049ce4c5106e1
fjf <116418179@qq.com>
2018-06-29 17:50:48 +0800

    Initial Commit

---
+++ FJFKeyboardHelperDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  FJFKeyboardHelperDemo
+//
+//  Created by fjf on 2018/6/29.
+//  Copyright © 2018年 fjf. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ FJFKeyboardHelperDemo/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  FJFKeyboardHelperDemo
+//
+//  Created by fjf on 2018/6/29.
+//  Copyright © 2018年 fjf. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
